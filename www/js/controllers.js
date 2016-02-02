1b3394178dca1c3b15d7ebc0d1198156a1a8be5d
nvoleau <voleau@gmail.com>
2016-02-02 06:12:23 +0100

    pouchdb

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,65 +8,61 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('mesEntrainementsCtrl', function($scope, $ionicPopup, PouchDBListener) {
 
-$scope.entrainements = [];
- $scope.create = function() {
-   $ionicPopup.prompt({
-     title: 'Un nouvel Entrainement',
-     inputType: 'text'
-   })
-   .then(function(result) {
-     console.log(result);
-     if(result) {
-       //if($scope.hasOwnProperty("Entrainement") !== true) {
-        // $scope.entrainements = [];
-       //}
-       localDB.post({title: result, done: false})
-       .then(function(response){
-             console.log(response);
-        }).catch(function(err){
-            console.log(err);
+.controller('mesEntrainementsCtrl', function($scope, $ionicModal, ionicPlatform, entrainementService) {
+	 var vm = this;
+
+    // Initialize the database.
+    $ionicPlatform.ready(function() {
+        entrainementService.initDB();
+
+        // Get all birthday records from the database.
+        entrainementService.getAllEntrainements().then(function(entrainements) {
+            vm.entrainements = entrainements;
         });
-     } else {
-       console.log("Action cancelled.");
-     }
-   });
- }
+    });
 
- $scope.update = function(entrainement) {
-   localDB.put({
-     _id: entrainement._id,
-     _rev: entrainement._rev,
-     title: entrainement.title,
-     done: entrainement.done
-   })
-   .then(function(result){
-     // You can set some action after the item was updated.
-   });
- }
- $scope.$on('add', function(event, entrainement) {
-   var add = true;
-    console.log('dans le add');
-   angular.forEach($scope.entrainements, function(value, key) {
-     if (value._id == entrainement._id) {
-       $scope.entrainements[key] = entrainement;
-       add = false;
-       return;
-     }
-   });
-   if (add) {
-     $scope.entrainements.push(entrainement);
-   }
- });
+    // Initialize the modal view.
+    $ionicModal.fromTemplateUrl('entrainement.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
 
- $scope.$on('delete', function(event, id) {
-   for(var i = 0; i < $scope.entrainements.length; i++) {
-     if($scope.entrainements[i]._id === id) {
-       $scope.entrainements.splice(i, 1);
-     }
-   }
- });
+    vm.showAddEntrainementModal = function() {
+        $scope.entrainement = {};
+        $scope.action = 'Add';
+        $scope.isAdd = true;
+        $scope.modal.show();           
+    };
+
+    vm.showEditEntrainementModal = function(entrainement) {
+        $scope.entrainement = entrainement;
+        $scope.action = 'Edit';
+        $scope.isAdd = false;          
+        $scope.modal.show();
+    };
+
+    $scope.saveEntrainement = function() {
+        if ($scope.isAdd) {
+            entrainementService.addEntrainement($scope.entrainement);              
+        } else {
+            entrainementService.updateEntrainement($scope.entrainement);               
+        }                       
+        $scope.modal.hide();
+    };
+
+    $scope.deleteEntrainement = function() {
+        entrainementService.deleteEntrainement($scope.entrainement);           
+        $scope.modal.hide();
+    };
+
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove(); 
+    });
+
+    return vm;
 
 })
    
--- www/js/services.js
+++ www/js/services.js
@@ -8,30 +8,88 @@ angular.module('app.services', [])
 
 }])
 //http://frontmag.no/artikler/utvikling/offline-data-synchronization-ionic
-.factory('PouchDBListener', ['$rootScope', function($rootScope) {
-	console.log('dans le factory');
- localDB.changes({
- 	since: 'now',
-  live: true,
-   continuous: true,
-   onChange: function(change) {
-   	console.log(change);
-     if (!change.deleted) {
-       $rootScope.$apply(function() {
-         localDB.get(change.id, function(err, doc) {
-           $rootScope.$apply(function() {
-             if (err) console.log(err);
-             $rootScope.$broadcast('add', doc);
-           })
-         });
-       })
-     } else {
-       $rootScope.$apply(function() {
-         $rootScope.$broadcast('delete', change.id);
-       });
-     }
-   }
- });
- return true;
-}]);
+.factory('EntrainementService', ['$q', EntrainementService]);
 
+function EntrainementService($q) {  
+    var _db;    
+
+    // We'll need this later.
+    var _entrainements;
+
+    return {
+        initDB: initDB,
+
+        // We'll add these later.
+        getAllEntrainements: getAllEntrainements,
+        addEntrainement: addEntrainement,
+        updateEntrainement: updateEntrainement,
+        deleteEntrainement: deleteEntrainement
+    };
+
+    function initDB() {
+        // Creates the database or opens if it already exists
+        _db = new PouchDB('shoot');
+    };
+
+    function addEntrainement(entrainement) {  
+      return $q.when(_db.post(entrainement));
+    };
+    function updateEntrainement(entrainement) {  
+    return $q.when(_db.put(entrainement));
+    };
+    function deleteEntrainement(entrainement) {  
+    return $q.when(_db.remove(entrainement));
+    };
+    function getAllEntrainements() {  
+      if (!_entrainements) {
+         return $q.when(_db.allDocs({ include_docs: true}))
+              .then(function(docs) {
+
+                  // Each row has a .doc object and we just want to send an 
+                  // array of birthday objects back to the calling controller,
+                  // so let's map the array to contain just the .doc objects.
+                  _entrainements = docs.rows.map(function(row) {
+                      // Dates are not automatically converted from a string.
+                      //row.doc.Date = new Date(row.doc.Date);
+                      return row.doc;
+                  });
+
+                  // Listen for changes on the database.
+                  _db.changes({ live: true, since: 'now', include_docs: true})
+                     .on('change', onDatabaseChange);
+
+                  return _entrainements;
+              });
+      } else {
+          // Return cached data as a promise
+          return $q.when(_entrainements);
+      }
+    };
+}
+
+function onDatabaseChange(change) {  
+    var index = findIndex(_entrainements, change.id);
+    var entrainement = _entrainements[index];
+
+    if (change.deleted) {
+        if (entrainement) {
+            _entrainements.splice(index, 1); // delete
+        }
+    } else {
+        if (entrainement && entrainement._id === change.id) {
+            _entrainements[index] = change.doc; // update
+        } else {
+            _entrainements.splice(index, 0, change.doc) // insert
+        }
+    }
+}
+
+// Binary search, the array is by default sorted by _id.
+function findIndex(array, id) {  
+    var low = 0, high = array.length, mid;
+    while (low < high) {
+    mid = (low + high) >>> 1;
+    array[mid]._id < id ? low = mid + 1 : high = mid
+    }
+    return low;
+}
