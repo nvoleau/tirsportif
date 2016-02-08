b031318b162df98665cc46a73a5c1fe9bcd03669
nvoleau <n.voleau@groupe-pomona.fr>
2016-02-08 16:45:45 +0100

    tirs en competition

--- www/js/controllersComp.js
+++ www/js/controllersComp.js
@@ -2,6 +2,9 @@ angular.module('app.controllersComp', [])
 
 .controller('competitionsCtrl', function($scope, $ionicModal,$ionicPlatform,competitionService) {
 
+
+var points=[];
+
     //$scope.competitions = competitions;
     $ionicPlatform.ready(function() {
        	competitionService.initDB(); 
@@ -48,10 +51,70 @@ angular.module('app.controllersComp', [])
     };
 
 
+var shoot = null;
+var evt = null;
+
     $scope.startCompet = function() {
-        console.log("Start")
+        shoot = 1;
+        evt = 1;
+        var tir = {};
+        tir.evt=0;
+        tir.action="Start";
+        tir.date = new Date();
+        points.push(tir);
+
+        $scope.point = 0;
+        console.log("Start " + shoot);
         $scope.modalStartCompet.show();           
     };
+
+   
+ 
+
+    $scope.nextShoot = function() {
+        var tir = {};
+        tir.action="tir";
+        tir.evt = evt;
+        tir.nb = shoot;        
+        tir.points = $scope.point;
+        tir.date = new Date();
+
+        //on reinit
+        $scope.point = 0;
+        shoot=shoot + 1;
+        evt = evt + 1;     
+
+        console.log(tir);
+        points.push(tir);
+        console.log(points);
+    };
+
+
+
+    $scope.setPauseDeb = function() {
+        var tir = {};
+        tir.action="Pause";
+        tir.evt = evt;
+        tir.date = new Date();
+        //on reinit
+        $scope.point = 0;
+        evt = evt + 1;
+        points.push(tir);
+        console.log(points);
+    };
+
+    $scope.setPauseFin = function() {
+        var tir = {};
+        tir.action="Pause";
+        tir.evt = evt;
+        tir.date = new Date();
+        //on reinit
+        $scope.point = 0;
+        evt = evt + 1;
+        points.push(tir);
+        console.log(points);
+    };
+
 
     $scope.setPoint = function(nb) {
         console.log("nb point "+nb); 
